e7617ee7959129817b1b2a70d578eed3a981353d
Antonio-RF <antoniidaressurreicao@gmail.com>
2025-03-20 22:48:55 -0300

    Update

--- Programação_2/Teste-A0-2023/fila.c
+++ Programação_2/Teste-A0-2023/fila.c
@@ -24,11 +24,23 @@ int fila_insere(struct nodo_t *noticia, struct fila_t *fila) {
         noticia->prox = NULL;
     }
     else {
+        //ERRO AQUI TALVEZ!
         fila->fim->prox = noticia;
         fila->fim = noticia;
         noticia->prox = NULL;
     }
     fila->num++;
+    //printf("NUmero de integrantes fila: %d\n", fila->num);
+    //conferindo...
+    struct nodo_t *aux = fila->prim;
+        while(aux != NULL) {
+            printf("-------------------------------------------\n");
+            printf("Título: %s\n", aux->titulo);
+            printf("Texto: %s\n", aux->texto);
+            printf("-------------------------------------------\n");
+            aux = aux->prox;
+        }
+    //conferindo...
 }
 
 int fila_remove(struct nodo_t *noticia, struct fila_t *fila) {
@@ -37,10 +49,8 @@ int fila_remove(struct nodo_t *noticia, struct fila_t *fila) {
     
     struct nodo_t *aux;
     aux = fila->prim->prox;
-    /*printa notícia*/
 
     //free na notícia.
-    free(noticia->prox);
     free(noticia);
 
     fila->prim = aux;
@@ -53,3 +63,132 @@ int fila_remove(struct nodo_t *noticia, struct fila_t *fila) {
 
     return 0;
 }
+
+void printa_edicao(struct nodo_t *noticia1, struct nodo_t *noticia2, int controle) {
+    //Controle define se tem duas notícias (controle=1) ou somente uma notícia (controle=0).
+    if (controle == 1) {
+        printf("=======================================================\n");
+        printf("%s\n", noticia1->titulo);
+        printf("");
+        printf("%s\n", noticia1->texto);
+        printf("");
+        printf("==\n");
+        printf("");
+        printf("%s\n", noticia2->titulo);
+        printf("");
+        printf("%s\n", noticia2->texto);
+        printf("=======================================================\n");
+    }
+    if (controle == 0) {
+        printf("=======================================================\n");
+        printf("%s\n", noticia1->titulo);
+        printf("");
+        printf("%s\n", noticia1->texto);
+        printf("");
+        printf("=======================================================\n");
+    }
+}
+
+void seleciona_noticia(struct fila_t *breaknews, struct fila_t *informe) {
+    if (breaknews->num >= 2) {
+        printa_edicao(breaknews->prim, breaknews->prim->prox, 1);
+        //Removendo os dois primeiros elementos da fila:
+        fila_remove(breaknews->prim, breaknews);
+        fila_remove(breaknews->prim, breaknews);
+    }
+    else if (breaknews->num == 1 && informe->num > 0) {
+        printa_edicao(breaknews->prim, informe->prim, 1);
+        //Removendo os dois primeiros elementos da fila:
+        fila_remove(breaknews->prim, breaknews);
+        fila_remove(informe->prim, informe);
+    }
+    else if (breaknews->num == 0 && informe->num >= 2) {
+        printa_edicao(informe->prim, informe->prim, 1);
+        //Removendo os dois primeiros elementos da fila:
+        fila_remove(informe->prim, informe);
+        fila_remove(informe->prim, informe);
+    }
+    else if (breaknews-> num == 1 && informe->num == 0) {
+        printa_edicao(breaknews->prim, breaknews->prim, 0);
+        //Removendo os dois primeiros elementos da fila:
+        fila_remove(breaknews->prim, breaknews);
+    }
+    else if (breaknews->num == 0 && informe->num == 1) {
+        printa_edicao(informe->prim, informe->prim, 0);
+        //Removendo os dois primeiros elementos da fila:
+        fila_remove(informe->prim, informe);
+    }
+    else {
+        printf("Esta edição foi pulada por falta de notícias!\n");
+    }
+}
+
+void atualiza_filas(struct fila_t *breaknews, struct fila_t *informes) {
+    //Atualizando as breaknews:
+    if (breaknews->num > 0) {
+        struct nodo_t *aux = breaknews->prim;
+        while (aux != NULL) {
+            aux->idade++;
+
+            //se for maior, ele irá retirar da fila.
+            if (aux->idade > 3) {
+                struct nodo_t *aux2 = aux;
+                aux = aux->prox;
+                free(aux2);
+            }
+            else {
+                aux = aux->prox;
+            }
+        }
+    }
+    //Atualizando os informes:
+    if (informes->num > 0) {
+        struct nodo_t *aux = informes->prim;
+        while (aux != NULL) {
+            aux->idade++;
+
+            //se for maior, ele irá retirar da fila.
+            if (aux->idade > 3) {
+                struct nodo_t *aux2 = aux;
+                aux = aux->prox;
+                free(aux2);
+            }
+            else {
+                aux = aux->prox;
+            }
+        }
+    }
+}
+
+void lista_imprime(struct fila_t *breaknews, struct fila_t *informes) {
+    printf("==============================================================\n");
+    printf("SOBREVIVENTES NAS DUAS LISTAS: \n");
+    printf("==============================================================\n");
+    printf("BREAKNEWS: \n");
+    if (breaknews->prim != NULL) {
+        struct nodo_t *aux = breaknews->prim;
+        while(aux != NULL) {
+            printf("-------------------------------------------\n");
+            printf("Título: %s\n", aux->titulo);
+            printf("Texto: %s\n", aux->texto);
+            printf("-------------------------------------------\n");
+            aux = aux->prox;
+        }
+    }
+    printf("==============================================================\n");
+    printf("INFORMES: \n");
+    if (informes->prim != NULL) {
+        struct nodo_t *aux2 = informes->prim;
+        while(aux2 != NULL) {
+            printf("-------------------------------------------\n");
+            printf("Título: %s\n", aux2->titulo);
+            printf("Texto: %s\n", aux2->texto);
+            printf("-------------------------------------------\n");
+            aux2 = aux2->prox;
+        }
+        free(aux2);
+    }
+    printf("==============================================================\n");
+}
+
+
--- Programação_2/Teste-A0-2023/fila.h
+++ Programação_2/Teste-A0-2023/fila.h
@@ -21,5 +21,9 @@ struct fila_t *cria_fila();
 struct nodo_t *cria_nodo(char *titulo, char *texto);
 int fila_insere(struct nodo_t *noticia, struct fila_t *fila);
 int fila_remove(struct nodo_t *noticia, struct fila_t *fila);
+void printa_edicao(struct nodo_t *noticia1, struct nodo_t *noticia2, int controle);
+void seleciona_noticia(struct fila_t *breaknews, struct fila_t *informe);
+void atualiza_filas(struct fila_t *breaknews, struct fila_t *informes);
+void lista_imprime(struct fila_t *breaknews, struct fila_t *informes);
 
 #endif
