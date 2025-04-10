62617a1021c1299e99fce9468a73bb063cd104a3
Antonio-RF <antoniidaressurreicao@gmail.com>
2025-04-09 22:19:51 -0300

    Update Trab1

--- Algoritmos_e_Estruturas_de_Dados_3/Trabalho1/redblack.c
+++ Algoritmos_e_Estruturas_de_Dados_3/Trabalho1/redblack.c
@@ -7,39 +7,45 @@ struct nodo *cria_nodo(struct nodo *no, int chave) {
 	n->chave = chave;
 	n->esq = NULL;
 	n->dir = NULL;
-	//Se o nó que foi passado é NULL = Não tem pai.
-	//Se não for NULL = Juntar pai e filho:
+	//Se o nó que foi passado é NULL = Não tem pai:
 	if (no == NULL)
 		n->pai = NULL;
-	else
+	//Se não for NULL = Juntar pai e filho:
+	else {
 		n->pai = no;
+		if (no->chave > n->chave)
+			no->esq = n;
+		else if (no->chave < n->chave)
+			no->dir = n;
+	}
 	return n;
 }
 
-struct nodo *inserir(struct nodo *no, int chave) {
-	if (no == NULL) 
-		return cria_nodo(no, chave);
+struct nodo *inserir(struct nodo *anterior, struct nodo *no, int chave) {
+	if (no == NULL)
+		return cria_nodo(anterior, chave);
 	if (no->chave > chave)
-		return inserir(no->esq, chave);
+		return inserir(no, no->esq, chave);
 	// AINDA NÃO DEFINI QUAL LADO VAI SE FOR IGUAL!!!
-	if (no->chave < chave) 
-		return inserir(no->dir, chave);
-	return no;
+	if (no->chave < chave)
+		return inserir(no, no->dir, chave);
+	return NULL;
 }
 
 void print_tree(struct nodo *no) {
 	if (no == NULL)
-		return
+		return;
 	print_tree(no->esq);
-	printf("%.d", no->chave);
+	printf("%.d ", no->chave);
 	print_tree(no->dir);
 }
 
 int main() {
 	struct nodo *raiz = cria_nodo(NULL, 10);
-	inserir(raiz, 7);
-	inserir(raiz, 11);
-	inserir(raiz, 6);
-	inserir(raiz, 8);
+	inserir(NULL, raiz, 7);
+	inserir(NULL, raiz, 11);
+	inserir(NULL, raiz, 6);
+	inserir(NULL, raiz, 8);
 	print_tree(raiz);
+	printf("\n");
 }
